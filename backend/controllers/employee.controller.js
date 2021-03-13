28ab24a967f24981e4350b69ed9b70d9c9196f3f
Alex Bellomo <alex.bellomo.ab@gmail.com>
2021-03-12 21:32:30 -0500

    Ran linter

--- backend/controllers/employee.controller.js
+++ backend/controllers/employee.controller.js
@@ -3,18 +3,18 @@ const axios = require('axios')
 const Order = db.order
 
 exports.getAllDrones = (req, res) => {
-    axios.get('http://drones.17-356.isri.cmu.edu/api/airbases/team3')
+  axios.get('http://drones.17-356.isri.cmu.edu/api/airbases/team3')
     .then(res => {
-      var requests = [];
-      for (var i = 0; i < res.data.drones.length; i++) {
+      const requests = []
+      for (let i = 0; i < res.data.drones.length; i++) {
         requests.push(axios.get('http://drones.17-356.isri.cmu.edu/api/drones/' + res.data.drones[i]))
-      } 
+      }
 
       return axios.all(requests)
     })
     .then(responses => {
-      var drones = [];
-      for (var i = 0; i < responses.length; i++) {
+      const drones = []
+      for (let i = 0; i < responses.length; i++) {
         drones.push(responses[i].data)
       }
       res.status(200).send({
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -120,41 +120,42 @@ exports.removeFromOrder = (req, res) => {
 }
 
 // Create a transaction
-function createTransaction(total_cost) {
+function createTransaction (totalCost) {
   return axios.post('http://credit.17-356.isri.cmu.edu/api/transactions', {
     companyId: 'team3',
-    amount: total_cost
+    amount: totalCost
   }).then(res => res.data.id)
 }
 
 // Get our company's drones
-function getDrones() {
+function getDrones () {
   return axios.get('http://drones.17-356.isri.cmu.edu/api/airbases/team3')
-  .then(res => res.data.drones)
+    .then(res => res.data.drones)
 }
 
 // Get a drone's status
-function getDroneStatus(drone_id) {
-  return axios.get('http://drones.17-356.isri.cmu.edu/api/drones/' + drone_id)
-  .then(res => res.data.current_delivery)
+function getDroneStatus (droneId) {
+  return axios.get('http://drones.17-356.isri.cmu.edu/api/drones/' + droneId)
+    .then(res => res.data.current_delivery)
 }
 
 // Process a transaction
-function processTransaction(transaction_id, username, credit_card) {
-  return axios.post('http://credit.17-356.isri.cmu.edu/api/transactions/' + transaction_id + '/process', {
-                  id: transaction_id,
-                  customer_details: username,
-                  credit_card: credit_card })
-  .then(res => res.data.status)
+function processTransaction (transactionId, username, creditCard) {
+  return axios.post('http://credit.17-356.isri.cmu.edu/api/transactions/' + transactionId + '/process', {
+    id: transactionId,
+    customer_details: username,
+    credit_card: creditCard
+  })
+    .then(res => res.data.status)
 }
 
 // Send a drone
-function sendDrone(drone_id) {
-  return axios.put('http://drones.17-356.isri.cmu.edu/api/drones/' + drone_id + '/send', {
+function sendDrone (droneId) {
+  return axios.put('http://drones.17-356.isri.cmu.edu/api/drones/' + droneId + '/send', {
     lat: 40.44,
     lon: -79.94
   })
-  .then(res => res.data.current_delivery)
+    .then(res => res.data.current_delivery)
 }
 
 exports.checkout = (req, res) => {
@@ -173,39 +174,38 @@ exports.checkout = (req, res) => {
         return res.status(404).send({ message: 'Order Not found.' })
       }
 
-      // Create  and process transaction        
+      // Create  and process transaction
       createTransaction(order.total_cost)
-      .then(transaction_id => {
-        return processTransaction(transaction_id, req.body.username, req.body.credit_card)
-      })
-      .then(processed => {
-        return getDrones()
-      })
-      .then(drone_ids => {
+        .then(transactionId => {
+          return processTransaction(transactionId, req.body.username, req.body.credit_card)
+        })
+        .then(processed => {
+          return getDrones()
+        })
+        .then(droneIds => {
         // Find available drone
-        var i = 0
-        var drone = -1
-        while (drone === -1) {
-          i++
-          i = i % drone_ids.length
-          drone = getDroneStatus(drone_ids[i])
-          .then(drone_status => {
-            if (drone_status == null) return drone_ids[i]
-            return -1
-          })
-        }
-        return i
-      })
-      .then(drone => {
-        return sendDrone(drone)
-      })
-      .then(result => {
-        return res.status(200).send({ message: "Order completed." })
-      })
-      .catch(err => {
-        return res.status(500).send({ message: err })
-      })
-
+          let i = 0
+          let drone = -1
+          while (drone === -1) {
+            i++
+            i = i % droneIds.length
+            drone = getDroneStatus(droneIds[i])
+              .then(droneStatus => {
+                if (droneStatus == null) return droneIds[i]
+                return -1
+              })
+          }
+          return i
+        })
+        .then(drone => {
+          return sendDrone(drone)
+        })
+        .then(result => {
+          return res.status(200).send({ message: 'Order completed.' })
+        })
+        .catch(err => {
+          return res.status(500).send({ message: err })
+        })
     })
     .catch(err => {
       return res.status(500).send({ message: err.message })
