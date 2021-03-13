96b27504e5946f8afa7c7f8833a083a75fc61de5
Alex Bellomo <alex.bellomo.ab@gmail.com>
2021-03-12 20:30:08 -0500

    Added checkout API integration

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const db = require('../models')
+const axios = require('axios')
 const Order = db.order
 const Product = db.product
 const OrderProduct = db.orderproduct
@@ -118,7 +119,46 @@ exports.removeFromOrder = (req, res) => {
     })
 }
 
+// Create a transaction
+function createTransaction(total_cost) {
+  return axios.post('http://credit.17-356.isri.cmu.edu/api/transactions', {
+    companyId: 'team3',
+    amount: total_cost
+  }).then(res => res.data.id)
+}
+
+// Get our company's drones
+function getDrones() {
+  return axios.get('http://drones.17-356.isri.cmu.edu/api/airbases/team3')
+  .then(res => res.data.drones)
+}
+
+// Get a drone's status
+function getDroneStatus(drone_id) {
+  return axios.get('http://drones.17-356.isri.cmu.edu/api/drones/' + drone_id)
+  .then(res => res.data.current_delivery)
+}
+
+// Process a transaction
+function processTransaction(transaction_id, username, credit_card) {
+  return axios.post('http://credit.17-356.isri.cmu.edu/api/transactions/' + transaction_id + '/process', {
+                  id: transaction_id,
+                  customer_details: username,
+                  credit_card: credit_card })
+  .then(res => res.data.status)
+}
+
+// Send a drone
+function sendDrone(drone_id) {
+  return axios.put('http://drones.17-356.isri.cmu.edu/api/drones/' + drone_id + '/send', {
+    lat: 40.44,
+    lon: -79.94
+  })
+  .then(res => res.data.current_delivery)
+}
+
 exports.checkout = (req, res) => {
+  // This is bad. Sorry.
   Order.findOne({
     where: {
       username: req.body.username,
@@ -133,19 +173,42 @@ exports.checkout = (req, res) => {
         return res.status(404).send({ message: 'Order Not found.' })
       }
 
-      // Create Transaction
-      // Send to API
-
-      // Get all drones
-      // Choose one
-      // Send it
-
-      res.status(200).send({
-        message: 'Order placed sucessfully.'
+      // Create  and process transaction        
+      createTransaction(order.total_cost)
+      .then(transaction_id => {
+        return processTransaction(transaction_id, req.body.username, req.body.credit_card)
       })
+      .then(processed => {
+        return getDrones()
+      })
+      .then(drone_ids => {
+        // Find available drone
+        var i = 0
+        var drone = -1
+        while (drone === -1) {
+          i++
+          i = i % drone_ids.length
+          drone = getDroneStatus(drone_ids[i])
+          .then(drone_status => {
+            if (drone_status == null) return drone_ids[i]
+            return -1
+          })
+        }
+        return i
+      })
+      .then(drone => {
+        return sendDrone(drone)
+      })
+      .then(result => {
+        return res.status(200).send({ message: "Order completed." })
+      })
+      .catch(err => {
+        return res.status(500).send({ message: err })
+      })
+
     })
     .catch(err => {
-      res.status(500).send({ message: err.message })
+      return res.status(500).send({ message: err.message })
     })
 }
 
