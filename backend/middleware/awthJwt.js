7b307bcd194f717c4931fb99f7f6399b05074b7d
Alex Bellomo <alex.bellomo.ab@gmail.com>
2021-03-16 14:11:41 -0400

    Added token to request instead of retreiving from cookie

--- backend/middleware/awthJwt.js
+++ backend/middleware/awthJwt.js
@@ -4,7 +4,7 @@ const db = require('../models')
 const User = db.user
 
 const verifyToken = (req, res, next) => {
-  const token = req.cookies.token || ''
+  const token = req.token || ''
 
   if (!token) {
     return res.status(403).send({
