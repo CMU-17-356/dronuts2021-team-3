d065eb371a9127dc1dfff124906a26f3eb7f7597
Pranav Prasad <pranav.prasad999@gmail.com>
2021-03-26 22:25:28 +0530

    Removed unused commented code

--- frontend/src/components/Login/SignOut.js
+++ frontend/src/components/Login/SignOut.js
@@ -24,7 +24,7 @@ export default class SignOut extends Component {
     cookies.remove('token')
     
     this.timed_out = setTimeout(() => this.setState({ redirect: true }), 3000)
-    
+
     return this.state.redirect
     ? <Redirect to="/login" />
     : <div>
@@ -33,45 +33,3 @@ export default class SignOut extends Component {
   }
 
 }
-
-
-
-
-// const Login = () => {
-//   return (
-//     <div className="login">
-//         <div className="login-left">
-//             <div></div>
-//             <div>
-//                 <h2> Looking for a donut delivered by a drone?</h2>
-//                 <img className="login-logo" src={Dronut_Logo} alt=""></img>
-//                 <h2> is here for you! </h2>
-//             </div>
-//             <div></div>
-//         </div>
-//         <div id="login-right" className="login-form">
-//             <div></div>
-//             <div>
-//                 <div className="login-header"><h2>Login</h2></div>
-//                 <div className="login-username"><input type="text" placeholder="Enter Username"></input></div>
-//                 <div className="login-password"><input type="password" id="pass" name="password"
-//             minlength="8" required placeholder="Enter Password"></input></div>
-//                 <div className="create-account">
-//                 <NavLink
-//                   className="navbar-item"
-//                   activeClassName="is-active"
-//                   to="/register"
-//                   exact
-//               ><p>Haven't made an account? Sign up here</p></NavLink></div>
-//                 <div className="button-container">
-//                     <div></div>
-//                     <div className="login-button"><a href="/#"><h3>Sign In</h3></a></div></div>
-//                     <div></div>
-//            </div>
-//            <div></div>
-//         </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
