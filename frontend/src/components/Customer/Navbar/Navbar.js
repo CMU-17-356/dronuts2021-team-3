38039b5bc5d1ebf3bd9595b4e49fbb90723aa213
Alex Bellomo <alex.bellomo.ab@gmail.com>
2021-03-31 16:26:51 -0400

    Done navbar customer

--- frontend/src/components/Customer/Navbar/Navbar.js
+++ frontend/src/components/Customer/Navbar/Navbar.js
@@ -5,12 +5,28 @@ import { NavLink } from 'react-router-dom';
 // component imports
 
 const NavbarCustomer = () => {
+  function openLinks() {
+    var x = document.getElementById("nav-cust-items-group");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
+
   return (
     <div className="navbar-cust">
         <div id="nav-cust-left">
-            <h4><a href="/#"><img className="nav-cust-logo" src={Dronut_Logo} alt=""></img></a></h4>
+            <a href="/#"><img className="nav-cust-logo" src={Dronut_Logo} alt=""></img></a>
         </div>
+
         <div id="nav-cust-right" className="nav-cust-menu">
+          <button class="icon" onClick={() => openLinks()}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </button>
+          <div id="nav-cust-items-group">
             <div className="nav-cust-items"><a href="/#"><h3>Menu</h3></a></div>
             <div className="nav-cust-items">
             <NavLink
@@ -33,6 +49,7 @@ const NavbarCustomer = () => {
                   to="/checkout"
                   exact
               ><h3>Checkout</h3></NavLink></div>            
+          </div>
         </div>
     </div>
   );
