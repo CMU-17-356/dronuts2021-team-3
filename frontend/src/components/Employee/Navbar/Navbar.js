df2b281fd4c40a30fb669254c4a11594fd335c91
Alex Bellomo <alex.bellomo.ab@gmail.com>
2021-03-31 17:30:45 -0400

    Employee navbar

--- frontend/src/components/Employee/Navbar/Navbar.js
+++ frontend/src/components/Employee/Navbar/Navbar.js
@@ -5,35 +5,51 @@ import { NavLink } from 'react-router-dom';
 // component imports
 
 const NavbarEmployee = () => {
+  function openLinks() {
+    var x = document.getElementById("nav-cust-items-group");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
+
   return (
     <div className="navbar-emp">
-        <div id="nav-emp-left">
-            <h4><a href="/#"><img className="nav-emp-logo" src={Dronut_Logo} alt=""></img></a></h4>
-        </div>
-        <div id="nav-emp-right" className="nav-emp-menu">
-            <div className="nav-emp-items">
-              <NavLink
-                  className="navbar-item"
-                  activeClassName="is-active"
-                  to="/employee/orders"
-                  exact
-              ><h3>Orders</h3></NavLink></div>
-            <div className="nav-emp-items">
+      <div id="nav-emp-left">
+          <a href="/#"><img className="nav-emp-logo" src={Dronut_Logo} alt=""></img></a>
+      </div>
+      <div id="nav-emp-right" className="nav-emp-menu">
+        <button class="icon" onClick={() => openLinks()}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </button>
+        <div id="nav-cust-items-group">
+          <div className="nav-emp-items">
             <NavLink
-                  className="navbar-item"
-                  activeClassName="is-active"
-                  to="/employee/drones"
-                  exact
-              >
-              <h3>Drones</h3></NavLink></div>
-            <div className="nav-emp-items">
-            <NavLink
-                  className="navbar-item"
-                  activeClassName="is-active"
-                  to="/employee/login"
-                  exact
-              ><h3>Sign In</h3></NavLink></div>            
+                className="navbar-item"
+                activeClassName="is-active"
+                to="/employee/orders"
+                exact
+            ><h3>Orders</h3></NavLink></div>
+          <div className="nav-emp-items">
+          <NavLink
+                className="navbar-item"
+                activeClassName="is-active"
+                to="/employee/drones"
+                exact
+            >
+            <h3>Drones</h3></NavLink></div>
+          <div className="nav-emp-items">
+          <NavLink
+                className="navbar-item"
+                activeClassName="is-active"
+                to="/employee/login"
+                exact
+            ><h3>Sign In</h3></NavLink></div>            
         </div>
+      </div>
     </div>
   );
 };
